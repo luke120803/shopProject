d966d862f87d1754a2673cd55b0d201b687b77b4
Moizex <moisesroll2@gmail.com>
2024-11-18 15:01:24 -0400

    Adicionando urls, views e html do produto

--- core/urls.py
+++ core/urls.py
@@ -5,5 +5,9 @@ urlpatterns = [
     path('clientes/', views.cliente_list, name='cliente_list'),
     path('clientes/novo/', views.cliente_form, name='cliente_create'),
     path('clientes/<int:id>/', views.cliente_form, name='cliente_update'),
-    # Similar paths for `produtos` and `vendas`
+
+    path('produtos/', views.produto_list, name='produto_list'),
+    path('produtos/novo/', views.produto_form, name='produto_create'),
+    path('produtos/<int:id>/', views.produto_form, name='produto_update'),
+
 ]
